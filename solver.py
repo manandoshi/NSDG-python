4b131b0549555358c223efcb3362e617bae3bc8d
Manan Doshi <manan.doshi@iitb.ac.in>
2018-03-24 14:47:20 +0530

    added rho, pressure term computation in velocity NS

--- solver.py
+++ solver.py
@@ -7,7 +7,7 @@ class convectiveSolver(object):
                  xmin=-1.0, xmax=1.0, nx=3, mx=4,
                  ymin=-1.0, ymax=1.0, ny=3, my=4,
                  boundaries=None, init=None, 
-                 alpha=0.1, nu=0.1,
+                 alpha=0.1, nu=0.1, rho=1.0,
                  sample_x=30, sample_y=30,
                  Th=10, dt=0.1, exact=True, adv_flux="rusanov"):
 
@@ -15,6 +15,8 @@ class convectiveSolver(object):
         self.system = system(xmin, xmax, ymin, ymax, nx, ny, mx, my, exact=exact, num_samples_x=sample_x, num_samples_y=sample_y)
         self.dt, self.Th = dt, Th
         self.k = {'T':alpha, 'u':nu, 'v':nu }
+        self.rho = rho
+
         self.fluxType = adv_flux
 
         #Adding main variables to system
@@ -43,9 +45,16 @@ class convectiveSolver(object):
             product = product * args[key]
         return product
     
-    def advectionDiffusion(self,args,p):
-        return self.diffusion(args,p) - self.advection(args,p)
+    def compute_dpdt(self,args,p):
+        if p == 'u':
+            return self.diffusion(args,p) - self.advection(args,p) - (1.0/self.rho)*selp.properties('dPdx')
 
+        if p == 'v':
+            return self.diffusion(args,p) - self.advection(args,p) - (1.0/self.rho)*selp.properties('dPdy')
+        else:
+            return self.diffusion(args,p) - self.advection(args,p)
+
+    
     def diffusion(self, args, p):
         px, py = args['d2'+p+'dx2'], args['d2'+p+'dy2']
         return self.k[p]*(px+py)
@@ -59,7 +68,7 @@ class convectiveSolver(object):
         t = args['T']
         return t + d*self.dt
 
-    #Function to compute required terms used in advection ad diffussion computation
+    #Function to compute required terms used in advection, diffussion and pressure term computation
     def compute_terms(self, p):
         #Advection terms
         self.system.add_property('u'+p, func=self.product, arg_params=['u',p])
@@ -80,9 +89,15 @@ class convectiveSolver(object):
         
         self.system.ddx('d2'+p+'dx2','d'+p+'dx')
         self.system.ddy('d2'+p+'dy2','d'+p+'dy')
+
+        #Pressure_term
+        if p == 'u':
+            self.system.ddx('dPdx','P')
+        elif p == 'v':
+            self.system.ddy('dPdy','P')
         
         #This shouldn't be here
-        self.system.add_property('d'+p+'dt', func=lambda args:self.advectionDiffusion(args, p), arg_params=['d2'+p+'dx2', 'd2'+p+'dy2','du'+p+'dx','dv'+p+'dy'])
+        self.system.add_property('d'+p+'dt', func=lambda args:self.compute_dpdt(args, p), arg_params=['d2'+p+'dx2', 'd2'+p+'dy2','du'+p+'dx','dv'+p+'dy'])
 
     def RK2_step(self):
         #step1
